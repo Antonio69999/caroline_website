87ae233d97ac30e3987b6cd77e38972d68c71799
neroma71 <neroma71@gmail.com>
2024-02-06 09:52:57 +0100

    add menu

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\ArticleRepository;
+use App\Repository\CategorieRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,13 +12,20 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(EntityManagerInterface $em, ArticleRepository $ar): Response
-    {
+    public function index(CategorieRepository $categorieRepository, ArticleRepository $articleRepository): Response
+{
+    // Récupérer la première catégorie (vous pouvez ajuster la logique selon vos besoins)
+    $categorie = $categorieRepository->findOneBy([], ['id' => 'ASC']);
+    $categories = $categorieRepository->findAll();
 
-        $articles = $ar->findAll();
+    // Récupérer les articles liés à la catégorie
+    $articles = $articleRepository->findBy(['categorie' => $categorie]);
+    
 
-        return $this->render('home/index.html.twig', [
-            'articles' => $articles,
-        ]);
-    }
+    return $this->render('home/index.html.twig', [
+        'categorie' => $categorie,
+        'articles' => $articles,
+        'categories' => $categories,
+    ]);
+}
 }
